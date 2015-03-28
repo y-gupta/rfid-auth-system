2cd506f1fae5c12dea55c80ad5a86f1d4baacb89
rohan_das <rohandas36@gmail.com>
2015-03-29 00:33:19 +0530

    printng format

--- GUI/printer/printer.cpp
+++ GUI/printer/printer.cpp
@@ -3,13 +3,48 @@
 printer::printer(){
     mkfifo("../printer/printer_pipe", 0666);
 }
+
 printer::~printer(){
 	// unlink("printer_pipe");
 }
-int printer::printit(string message)
-{
+
+string printer::print_user(user usr){
+	line="******************************************************";
+	string s;
+	s=s+"user ID :-";
+	int j=print_len-10-usr.user_name.length();
+	for(int i=0; i!=j&&j>0; i++) s=s+" ";
+	s=s+usr.user_name+"\nEntry No :-";
+	j=print_len-11-usr.entry_no.length();
+	for(int i=0; i!=j&&j>0; i++) s=s+" ";
+	s=s+usr.entry_no+"\nHostel Name :-";
+	j=print_len-14-usr.hostel_name.length();
+	for(int i=0; i!=j&&j>0; i++) s=s+" ";
+	s=s+usr.hostel_name+"\n";
+	return s;
+}
+
+int rebate(user The_user,string start, string end){
+	string s = line+"\n"+print_user(The_user)+"\n----Rebate Period----\n"+start+"\nTo\n"+end+"\n"+line+"\nTHANK YOU!!!\n.....";
+    fd = open("../printer/printer_pipe", O_WRONLY);
+	
+	if(fd<0){
+		cout<<"Cannot open pipe"<<endl;
+		return -1;
+	}
+
 	bzero(buf,1024);
-	memcpy(buf,message.c_str(),message.size());
+	memcpy(buf,s.c_str(),s.size());
+	write(fd,buf,1024); 
+	
+	
+	close(fd);
+	return 1;
+}
+	int extra_messing(user The_user,string amount){
+	
+	string s = line+"\n"+print_user(The_user)+"\nThis Coupon is worth\n"+amount+"\n"+line+"\nTHANK YOU!!!\n.....";
+	
 	
     fd = open("../printer/printer_pipe", O_WRONLY);
 	
@@ -17,10 +52,19 @@ int printer::printit(string message)
 		cout<<"Cannot open pipe"<<endl;
 		return -1;
 	}
+	
+	bzero(buf,1024);
+	memcpy(buf,s.c_str(),s.size());
 	write(fd,buf,1024); 
+	
+	
 	close(fd);
 	return 1;
 }
+
+
+
+
 //int main(int argc,char** argv){
 //printer tmt81;
 
--- GUI/printer/printer.py
+++ GUI/printer/printer.py
@@ -6,6 +6,8 @@ Epson = printer.Usb(0x04b8,0x0202)
 
 rfPath = "./printer_pipe"
 while (1) :
+	Epson.set(align='center', bold=True)
+	Epson.image("9795.png")
 	rp = open(rfPath, 'r')
 	message = rp.read()
 
@@ -14,3 +16,7 @@ while (1) :
 	Epson.text(message)
 
 	Epson.cut()
+
+
+
+
