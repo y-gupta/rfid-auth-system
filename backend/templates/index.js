a75e10b01552b67ee342e55d8a4361a9cd6b512d
Yash Gupta <yashg2@gmail.com>
2015-04-13 00:39:31 +0530

    template

--- backend/templates/index.js
+++ backend/templates/index.js
@@ -2,9 +2,9 @@ var fs=require('fs');
 var _=require('lodash');
 var Mustache=require('mustache');
 data=JSON.parse(fs.readFileSync('./data.json'),'utf8');
-var files=fs.readdirSync('./new');
-var menue = Mustache.render(fs.readFileSync('../menue.html','utf8'),data);
+var files=fs.readdirSync('./new/');
+var menue = fs.readFileSync('./menu.html','utf8');
 function compile(file){
   fs.writeFileSync('./compiled/'+file,Mustache.render(menue, data, {content:Mustache.render(fs.readFileSync('./'+file,'utf8'))} ));
 }
-_.each(files,function(file){compile('admin/'+file)});
+_.each(files,function(file){compile('new/'+file)});
