15e256a427e95aa43c20c97f60e6eacbc60a5417
Rohan Das <rohandas36@gmail.com>
2015-04-13 00:31:13 +0530

    index updated

--- backend/templates/index.js
+++ backend/templates/index.js
@@ -2,9 +2,9 @@ var fs=require('fs');
 var _=require('lodash');
 var Mustache=require('mustache');
 data=JSON.parse(fs.readFileSync('./data.json'),'utf8');
+var files=fs.readdirSync('./new');
+var menue = Mustache.render(fs.readFileSync('../menue.html','utf8'),data);
 function compile(file){
-  fs.writeFileSync('./compiled/'+file,Mustache.render(fs.readFileSync('./'+file,'utf8'), data));
+  fs.writeFileSync('./compiled/'+file,Mustache.render(menue, data, {content:Mustache.render(fs.readFileSync('./'+file,'utf8'))} ));
 }
-
-var files=fs.readdirSync('./admin');
 _.each(files,function(file){compile('admin/'+file)});
