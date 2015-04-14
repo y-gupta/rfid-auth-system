dffa58f04705e8cf69d80579ce605560de4083eb
Rishit Sanmukhani <rishitsanmukhani@gmail.com>
2015-04-14 17:06:55 +0530

    final changes

--- GUI/printer/printer.py
+++ GUI/printer/printer.py
@@ -1,23 +1,13 @@
-
-# from escpos import *
-# import os
-
-# Epson = printer.Usb(0x04b8,0x0202)
-
-# rfPath = "./printer_pipe"
-# while (1) :
-# 	rp = open(rfPath, 'r')
-# 	message = rp.read()
 from escpos import *
+import os
 
+Epson = printer.Usb(0x04b8,0x0202)
 rfPath = "./printer_pipe"
+
 while (1) :
-	Epson.set(align='center', bold=True)
-	Epson.image("9795.png")
+	Epson.set(align='center')
 	rp = open(rfPath, 'r')
 	message = rp.read()
-
-# 	Epson.text(message)
-
-# 	Epson.cut()
+	Epson.image("9795.png")
+	Epson.text(message)
 	Epson.cut()
--- GUI/rfid/rfid.cpp
+++ GUI/rfid/rfid.cpp
@@ -3,31 +3,24 @@
 #include <cstdlib>
 
 void RFID::init(){
-	cout<<"Creating pipe ";
     mkfifo("../rfid/rfid_pipe",0666);
     srand(0);
-
-    cout<<"pipe created ..."<<endl;
 }
 int64_t RFID::readCard(){
-    if(rand()%10 < 6)
+    if(rand()%10000 < 9999)
         return -1;
     else
         return 1;
-
 //    char buff[20];
 //    int fd = open("../rfid/rfid_pipe",O_WRONLY);
 //    assert(fd != -1 && "error opening");
-//    cout<<"writing..."<<endl;
 //    int bytes = write(fd,"GET",sizeof("GET"));
 //    close(fd);
 //    assert(bytes != -1 && "write successful");
-//    cout<<"writing complete"<<endl;
 
 
 //    fd = open("../rfid/rfid_pipe",O_RDONLY);
 //    assert(fd != -1 && "error opening");
-//    cout<<"reading..."<<endl;
 //    bytes = read(fd,buff,12);
 //    assert(bytes != -1 && "read successful");
 //    buff[12]=0;
