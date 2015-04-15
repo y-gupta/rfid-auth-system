7409be5846d6a6cda0ed9a3e5be15732358b1163
rohan_das <rohandas36@gmail.com>
2015-04-15 14:19:57 +0530

    final

--- backend/templates/index.js
+++ backend/templates/index.js
@@ -5,6 +5,6 @@ data=JSON.parse(fs.readFileSync('./data.json'),'utf8');
 var files=fs.readdirSync('./new/');
 var menue = fs.readFileSync('./menu.html','utf8');
 function compile(file){
-  fs.writeFileSync('./compiled/'+file,Mustache.render(menue, data, {content:Mustache.render(fs.readFileSync('./'+file,'utf8'))} ));
+  fs.writeFileSync('./compiled/'+file,Mustache.render(menue, data, {content:Mustache.render(fs.readFileSync('./'+file,'utf8'),data)} ));
 }
 _.each(files,function(file){compile('new/'+file)});
